ef7d377825f097bd25fdd558a13b39ecabb922d8
JenniferKarr <karr@prometheus.local>
2018-01-18 13:28:44 +0800

    Added expose_standard and expose_long to fakeCamera.py

--- python/mcsActor/fakeCamera.py
+++ python/mcsActor/fakeCamera.py
@@ -24,7 +24,33 @@ class FakeCamera(Camera):
 
         cmd.inform('cameraName=%s; readNoise=%0.2f' % (self.name, self.readNoise))
 
-    def expose_fake(self, cmd, filename, get_arc):
+    def expose_long(self, cmd, filename, get_arc):
+
+        """
+
+        Read an image file (and optionally an arc file) from disk, to mimic
+        a real exposure.
+
+        Args: 
+            cmd     - a Command object to report to. Ignored if None.
+            filename - FITS filename (minus FITS extension) of the file to read into
+            get_arc - flag for arc image. If get_arc=1, looks for filename_arc.fits
+
+        Returns 
+            the image
+            the arc image (optional)
+
+        """
+        
+        image=pyfits.getdata(filename+".fits",1)
+
+        if(get_arc==1):
+            arc_image=pyfits.getdata(filename+"_arc.fits",1)
+            return image,arc_iamge
+        else:
+            return image
+
+    def expose_standard(self, cmd, filename,get_arc):
 
         """
 
