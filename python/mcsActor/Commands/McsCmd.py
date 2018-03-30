dc8d941cc3c4df5c01b4b104604f04c3e41de248
Craig Loomis <cloomis@astro.princeton.edu>
2018-03-30 12:10:32 -0400

    Formatting cleanups.

--- python/mcsActor/Commands/McsCmd.py
+++ python/mcsActor/Commands/McsCmd.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-
-
 from __future__ import print_function
 from builtins import zip
 
@@ -10,8 +8,6 @@ from builtins import object
 import matplotlib
 import matplotlib.pyplot as plt
 import time
-
-matplotlib.use('Agg')
 
 import os
 import base64
