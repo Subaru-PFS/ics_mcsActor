211843ba87e3f0b6feb8dd9b489c420cf74908e3
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2018-04-26 10:07:51 +0800

    Adding source code for camera and shutter initialization.

---
+++ source/Canon50M/canoninit.sh
@@ -0,0 +1,1 @@
+/opt/EDTpdv/initcam -f ~pfs/canon-8960x5778.cfg
---
+++ source/Canon50M/shutterinit.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+rmmod pl2303
+modprobe usbserial
+insmod /home/pfs/mhs/devel/ics_mcsActor/source/ShutterDriver/pl2303.ko
+usermod -a -G dialout pfs
+shutter -o
+shutter -c
+
