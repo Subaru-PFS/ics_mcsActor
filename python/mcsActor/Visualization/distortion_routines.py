8893035e4f6582231e21353771d2b231cbb06d37
JenniferKarr <karr@prometheus.local>
2018-06-06 09:26:17 +0800

    quick update of file

--- python/mcsActor/Visualization/distortion_routines.py
+++ python/mcsActor/Visualization/distortion_routines.py
@@ -1,22 +1,36 @@
 
 
 import numpy as np
-import pyfits as pf
 import pylab as py
 import centroid as centroid
 from heapq import nsmallest
 import cv2
 from matplotlib.cm import plasma
 from scipy.stats import sigmaclip
+import astropy
+import matplotlib 
 
 
 def getImage(filename):
 
-    image=pf.getdata(filename)
+    """
+
+    Simple wrapper to read an image from file
+
+    """
+    
+    image=astropy.io.fits.getdata("MCST_010_001.fits")
+
     return image
 
 def getCentroids(image,fwhm,boxsize,thresh,scale,rl,rh,sl,sh):
 
+    """
+    
+    wrapper to run centroid command, and convert the output to arrays
+
+    """
+    
     #centroid 
     a=centroid.centroid_only(image.astype('<i4'),fwhm,thresh,boxsize,2,sl,sh,rl,rh,0)
     centroids=np.frombuffer(a,dtype='<f8')
@@ -37,34 +51,70 @@ def getCentroids(image,fwhm,boxsize,thresh,scale,rl,rh,sl,sh):
 
 def approximateCoords(x,y,region):
 
+    """
+
+    Get lower left position and rotation angle from a set of pin-hole mask
+    centroids.  This will likely break if the rotation angle is too high. 
+    
+    input
+
+    x,y: 1D numpy arrays of coordinates (pixel)
+
+    region: 4 element array with of the region of interest [x1,x2,y1,y2]
+
+    returns: x1,y1 of lower left corner, angle in radians
+
+    """
+
+    #find points in the ergion
+    
     ind=np.where((x > region[0]) & (x < region[1]) & (y > region[2]) & (y < region[3]))
 
+    #find the minimum and maximum distance from the origin
+    
     dd=x*x+y*y
 
     ind1=np.where(dd == dd.max())
     ind2=np.where(dd == dd.min())
 
+    #calculate the angle of rotation wrt Y axis
     
     angle=np.arctan((y[ind1]-y[ind2])/(x[ind1]-x[ind2]))-np.pi/4.
 
     return x[ind2],y[ind2],angle
 
 def maskinMM(smallBox):
-                        
-    #manually crop the close points
+
+    """
+
+    calculates the position of the holes in the pinhole mask, in mm
+    
+    set smallbox=1 to include the close spaced points, 0 to exclude
+
+    returns x,y of holes
+  
+    """
+    
+
     xx=[]
     yy=[]
 
-    #centre spots
+    #region of close points
+    
     pp1=np.array([6,13,8,15])+0.5
     pp3=np.array([28,35,8,15])+0.5
     pp2=np.array([17,24,30,37])+0.5
 
+    #the dimensions of teh large spaced points are 8mm distance, 43 points
+    
     for i in range(43):
         for j in range(43):
             iin=1
             ii=i
             jj=j
+
+            #remove points in the close-spaced region
+            
             if((ii > pp1[0]) & (ii < pp1[1]) & (jj > pp1[2]) & (jj < pp1[3])):
                iin=0
             if((ii > pp2[0]) & (ii < pp2[1]) & (jj > pp2[2]) & (jj < pp2[3])):
@@ -75,9 +125,13 @@ def maskinMM(smallBox):
             if((i==21) and (j==21)):
                 iin=0
             if(iin==1):
+               #convert from point number to mm
+                
                xx.append(ii*8)
                yy.append(jj*8)
 
+    #same thing for the close spaced points if desired; 2 mm separation, 25 points
+    
     if(smallBox==1):
         for i in range(25):
             for j in range(25):
@@ -100,42 +154,111 @@ def maskinMM(smallBox):
 
 def scaleCentroids(x,y,x1,y1,scale):
 
+    """
+
+    scale the centroids to mm at mask, and shift to the origin
+
+    input
+
+    x1,y1: lower left spot
+    x,y: coordinates of centroids
+    scale: scale factor
+
+    returns: transformed x,y
+
+    """
+    
     xc=(x-x1)/scale
     yc=(y-y1)/scale
 
     return xc,yc
 
 def scaleMask(xx,yy,angle,flip):
-    #apply any rotation (for matching purposes only)
-    xx=xx-168
-    yy=yy-168
 
+    """
+
+    rotate the mask as needed
+
+    input
+
+    xx,yy: hole positions
+    angle: rotation angle
+    flip: set to 1 to rotate by 90 degrees
+
+    returns: transformed x,y
+
+    """
+    
+    #apply any rotation (for matching purposes only)
+
+    #add 90 degrees to rotation
+    
     if(flip==1):
         angle=angle+np.pi/2
+
+    #shift to centre, rotate, shift back
+    xx=xx-168
+    yy=yy-168
         
     xnew=xx*np.cos(angle)-yy*np.sin(angle)
     ynew=xx*np.sin(angle)+yy*np.cos(angle)
-
-        
     
     xx=xnew+168
     yy=ynew+168
 
     return xx,yy
 
-def checkCentroids(xc,yc,xx,yy,cutrange,prefix):
+def checkCentroids(xc,yc,cutrange,prefix):
 
-    #quick plot of centroids
+    """
+
+    Quick plot of centroids to check results
+
+    input
+
+    xc,yc: centroid coordinates
+    cutrange: limit the region of plotting if needed (for bad data)
+    prefix: prefix for plots
+
+    returns: plot, to screen and file
+
+    """
+
+    #set figuresize
     py.clf()
-    py.scatter(xc,yc)
-    py.scatter(xx,yy,s=2,color='r')
-    py.savefig("/Users/karr/"+prefix+"_checkpoints.jpg")
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
 
+    #scatter plot
+    
+    py.scatter(xc,yc)
+
+    #set limit if desired
     if(cutrange==1):
         np.xlim([-8,344])
         np.ylim([-8,344])
+
+    #display and save
+    py.show()
+    py.savefig(prefix+"_checkpoints.jpg")
         
 def matchPoints(xc,yc,xx,yy,fx,fy,peak):    
+
+    """
+
+    match the image spots to the mask holes
+
+    input
+
+    xc,yc: centroids
+    xx,yy: hole coordinates
+    fx,fy: fwhms
+    peak: peak values
+
+    returns: xs,ys,fxs,fyx,peaks: measured variables matched to the hole positions
+
+    """
+
+    #create arrays
     
     xs=np.zeros((len(xx)))
     ys=np.zeros((len(yy)))
@@ -144,7 +267,6 @@ def matchPoints(xc,yc,xx,yy,fx,fy,peak):
     peaks=np.zeros((len(xx)))
     
     #match points between two sets (nearest neighbour)
-    #for i in range(len(xx)):
     for i in range(len(xx)):
         for j in range(len(xc)):
 
@@ -160,16 +282,52 @@ def matchPoints(xc,yc,xx,yy,fx,fy,peak):
 
 
 def checkMatched(xx,yy,xs,ys,prefix):
-    
-    #quick plot of matched points
-    py.clf()
-    py.scatter(xs,ys)
-    py.scatter(xx,yy,s=2,color='r')
-    py.savefig("/Users/karr/"+prefix+"_checkpoints1.jpg")
 
+    """
+
+    quick plotting routine for measured centroids and pinhole coordinates
+
+    input: 
+
+    xx,yy mask coordiantes
+    xs,ys: spot coordinates
+    prefix: prefix for image files
+
+    """
+
+    
+    #set image size
+    py.clf()
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+
+    #scatter plot: centroids in circles, mask in red dots
+    
+    py.scatter(xs,ys)
+    py.scatter(xx,yy,s=20,color='r')
+
+    #save and show
+    py.savefig(prefix+"_checkpoints1.jpg")
+    py.show()
 
 def simpleDistortion(xx,yy,xs,ys):    
 
+    """
+
+    Calculate the distortion map using opencv library
+
+    input: 
+
+    xx,yy mask  coordinates
+    xs,ys: spot coordinates
+
+    returns:
+
+    c,c1: distortion magnitude in mm and % of field
+    pts1,pts2: points with average removed
+    diffx,diffy: difference between pts2 and pts with translation/rotation removed
+
+    """
+    
     #subtract average to align
 
     nn=len(xs)
@@ -227,9 +385,25 @@ def simpleDistortion(xx,yy,xs,ys):
 
 def plotDistortion(c,c1,pts1,pts2,diffx,diffy,fxs,fys,peaks,limit,prefix):
 
-    if(limit > 0):
-        #a quick cludge to filter out bad quality points in poorly focussed images
+    """
+
+    Distortion plots
+
+    input:
+
+    c,c1,pts1,pts2,diffx,diffy: as returned by simpleDistortion
+    fxs,fyx: fwhms
+    peaks: peak values
+
+    limit: upper limit for filtering out bad data (in c units)
+
+    prefix: prefix for output files
+
+    """
+
+    #a quick kludge to filter out bad quality points in poorly focussed images
     
+    if(limit > 0):
         ind=np.where((c < limit) & (fxs < 10) & (fys < 10) & (fxs > 0) & (fys > 0) & (peaks != 0))
     else:
         ind=np.arange(len(c))
@@ -237,63 +411,89 @@ def plotDistortion(c,c1,pts1,pts2,diffx,diffy,fxs,fys,peaks,limit,prefix):
     #quiver plot
     
     py.clf()
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+
     py.quiver(pts1[0,ind,0],pts1[0,ind,1],-diffx[ind],-diffy[ind])
-    py.xlabel("x (mm)")
-    py.ylabel("y (mm)")
+    py.xlabel("x (mm)",fontsize=40)
+    py.ylabel("y (mm)",fontsize=40)
     py.title("Distortion")
-    #py.show()
-    py.savefig("/Users/karr/"+prefix+"_distortion.jpg")
+    py.show()
+    py.savefig(prefix+"_distortion.jpg")
 
     #t1=np.partition(c.flatten(), -2)[-2]
     #t2=np.partition(c1.flatten(), -2)[-2]
     
     #plot map in mm
     py.clf()
-    py.scatter(pts2[0,ind,0],pts2[0,ind,1],marker='s',c=c[ind],cmap='plasma')
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+    py.scatter(pts2[0,ind,0],pts2[0,ind,1],marker='s',c=c[ind],cmap='plasma',s=200)
     py.colorbar()
     py.title("Distortion (mm)")
-    py.xlabel("x (mm)")
-    py.ylabel("y (mm)")
-    py.savefig("/Users/karr/"+prefix+"_distortion_col.jpg")
+    py.xlabel("x (mm)",fontsize=40)
+    py.ylabel("y (mm)",fontsize=40)
+    py.show()
+    py.savefig(prefix+"_distortion_col.jpg")
 
     #plot map in %
     py.clf()
-    py.scatter(pts2[0,ind,0],pts2[0,ind,1],marker='s',c=c1[ind],cmap='plasma')
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+    py.scatter(pts2[0,ind,0],pts2[0,ind,1],marker='s',c=c1[ind]*4,cmap='plasma',s=200)
     py.colorbar()
     py.title("Distortion (% of field size)")
-    py.xlabel("x (mm)")
-    py.ylabel("y (mm)")
-    py.savefig("/Users/karr/"+prefix+"_distortion_col1.jpg")
+    py.xlabel("x (mm)",fontsize=40)
+    py.ylabel("y (mm)",fontsize=40)
+    py.show()
+    py.savefig(prefix+"_distortion_col1.jpg")
 
     py.clf()
-
-    #t1=np.partition(fxs[ind].flatten(), -2)[-2]
-    #t2=np.partition(fys[ind].flatten(), -2)[-2]
-  
 
 def plotVal(xs,ys,val,limit,plotrange,titl,prefix,suffix):
 
-    if(limit > 0):
-        #a quick cludge to filter out bad quality points in poorly focussed images
+    """
     
+    routine for scatter plot of a variable
+
+    input
+
+    xs,ys: coordinates
+
+    val: variable to plot
+    limit: upper limit to filter out of plots
+    plotrange: colour range in [vmin,vmax] format, or None for default
+    title: title for plot
+    prefix: prefix for output files
+    suffix: suffix for output files
+
+    """
+
+    #set the font sizes for labels
+    
+    matplotlib.rc('xtick', labelsize=20) 
+    matplotlib.rc('ytick', labelsize=20) 
+    
+    #a quick kludge to filter out bad quality points in poorly focussed images
+    if(limit > 0):
         ind=np.where((val < limit) & (val > 0) & (val > 0))
     else:
-        ind=np.arange(len(c))
+        ind=np.arange(len(val))
 
-    #plot FWHMs
-
+    #scatter plot, with or without ragne limit
+    
     py.clf()
+    fig=py.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+
     if(plotrange != None):
-        sc=py.scatter(xs[ind],ys[ind],c=val[ind],marker="s",cmap='Purples',lw=0,s=20,vmin=plotrange[0],vmax=plotrange[1])
+        sc=py.scatter(xs[ind],ys[ind],c=val[ind],marker="s",cmap='Purples',lw=0,s=200,vmin=plotrange[0],vmax=plotrange[1])
 
     else:
-        sc=py.scatter(xs[ind],ys[ind],c=val[ind],marker="s",cmap='Purples',lw=0,s=20)
+        sc=py.scatter(xs[ind],ys[ind],c=val[ind],marker="s",cmap='Purples',lw=0,s=200)
 
     py.colorbar(sc)
-    py.title(titl)
-    py.xlabel("X (mm)")
-    py.ylabel("Y (mm)")
-    py.savefig("/Users/karr/"+prefix+suffix+".jpg")
+    py.title(titl,fontsize=40)
+    py.xlabel("X (mm)",fontsize=40)
+    py.ylabel("Y (mm)",fontsize=40)
+    py.show()
+    py.savefig(prefix+suffix+".jpg")
 
     
 def to_fits(filename):
@@ -309,14 +509,28 @@ def to_fits(filename):
 
 def total_flux(xs,ys,image,x1,y1,scale):
 
+    """
+
+    Very simple estimate of total flux of a point. Sums up the values in a box,
+    subracting average background of image. 
+
+    input: 
+
+    xs,ys: coords of spots
+    image: image array
+    x1,y1: lower left spot coordinates
+    sclae: from pixel to mm at mask
+
+    returns: array of fluxes
+
+    """
+
+    
     #back to pixel coordinates
     xc=xs*scale+x1
     yc=ys*scale+y1
 
-    #simple photometry for total flux
-    
-    flux=total_flux(xc,yc,image,x1,y1,scale)
-
+    #estimate background
     
     ind=np.where(image > 0)
     
@@ -324,6 +538,7 @@ def total_flux(xs,ys,image,x1,y1,scale):
     back,a,b=sigmaclip(np.ravel(image[ind]))
     back=back.mean()
 
+    #initialize array
     
     flux=np.zeros((len(xc)))
 
@@ -336,7 +551,6 @@ def total_flux(xs,ys,image,x1,y1,scale):
         yy=np.round(yc[i])
         j1=int(xx)
         i1=int(yy)
-        #print("circle point ",j1,i1)
 
         for ii in range(-4,5):
             for jj in range(-4,5):
