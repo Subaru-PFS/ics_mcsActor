10f54b968c038e23f8227f5b99f29d522d9528be
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2021-11-20 20:37:43 -1000

    Fixing typo

--- source/Canon50M/canonexp.c
+++ source/Canon50M/canonexp.c
@@ -134,7 +134,7 @@ int WriteFitsImage(char *filename, int height, int width, u_char * image_p, int 
     fits_create_file(&fptr, filename, &status); /* create new FITS file */
     if (status != 0) {
     	fprintf(stderr, "Error: (%s:%s:%d) can not get create image %s in disk "
-    			".\n", __FILE__, __func__, __LINE__,filenmae);
+    			".\n", __FILE__, __func__, __LINE__,filename);
     	exit(1);
     }
 
